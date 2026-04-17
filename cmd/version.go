a105680c6b4dc4f8f130d66123fad52404f52d59
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2026-04-17 08:12:17 -0700

    change the entry point to support go install

--- cmd/version.go
+++ cmd/version.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"fmt"
 	"io"
+	"runtime/debug"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -32,5 +34,16 @@ func runVersion(w io.Writer) error {
 }
 
 func init() {
+	if info, ok := debug.ReadBuildInfo(); ok && version == "dev" {
+		if info.Main.Version != "" && info.Main.Version != "(devel)" {
+			version = strings.TrimPrefix(info.Main.Version, "v")
+		}
+		for _, s := range info.Settings {
+			if s.Key == "vcs.revision" && len(s.Value) >= 7 {
+				commit = s.Value[:7]
+				break
+			}
+		}
+	}
 	rootCmd.AddCommand(versionCmd)
 }
