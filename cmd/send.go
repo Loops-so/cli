a2c6f511979022a06c0c92d5f2089e4e41f8f5fb
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2026-04-27 11:24:08 -0700

    transactional: positional id

--- cmd/send.go
+++ cmd/send.go
@@ -3,8 +3,9 @@ package cmd
 import "github.com/spf13/cobra"
 
 var sendCmd = &cobra.Command{
-	Use:   "send",
+	Use:   "send <id>",
 	Short: "Send a transactional email",
+	Args:  cobra.ExactArgs(1),
 	RunE:  transactionalSendRunE,
 }
 
