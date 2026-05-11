926662e9702b90dd72291911fee5576033a9dc6b
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2026-05-11 14:08:28 -0700

    ascii art in version output

--- cmd/build_info.go
+++ cmd/build_info.go
@@ -10,6 +10,15 @@ var (
 	commit  = "none"
 )
 
+// includes a leading newline to make it easy to read the ascii art here in the source
+const versionHeader = `
+    __    ____  ____  ____  _____
+   / /   / __ \/ __ \/ __ \/ ___/
+  / /   / / / / / / / /_/ /\__ \
+ / /___/ /_/ / /_/ / ____/___/ /
+/_____/\____/\____/_/    /____/
+`
+
 func init() {
 	if info, ok := debug.ReadBuildInfo(); ok && version == "dev" {
 		if info.Main.Version != "" && info.Main.Version != "(devel)" {
@@ -22,4 +31,6 @@ func init() {
 			}
 		}
 	}
+	header := strings.TrimPrefix(versionHeader, "\n")
+	rootCmd.SetVersionTemplate(header + "\n{{with .Name}}{{printf \"%s \" .}}{{end}}{{printf \"version %s\" .Version}}\n")
 }
